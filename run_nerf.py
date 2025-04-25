d55958b20b087afac16f081785f494c2fc13f3f7
kenny <kenny_iap@126.com>
2025-04-25 07:55:37 +0000

    add comments in config_parser

--- run_nerf.py
+++ run_nerf.py
@@ -18,9 +18,11 @@ from load_deepvoxels import load_dv_data
 from load_blender import load_blender_data
 from load_LINEMOD import load_LINEMOD_data
 
-
+# 训练设备：GPU或CPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# 设置随机种子为0（固定值产生相同随机序列）
 np.random.seed(0)
+# 用于测试
 DEBUG = False
 
 
@@ -422,109 +424,150 @@ def config_parser():
 
     import configargparse
     parser = configargparse.ArgumentParser()
+    # 配置文件，parser会读取该文件中的参数，然后赋值给下面的参数
     parser.add_argument('--config', is_config_file=True, 
                         help='config file path')
+    # 实验名字
     parser.add_argument("--expname", type=str, 
                         help='experiment name')
+    # log路径，保存训练日志和网络参数
     parser.add_argument("--basedir", type=str, default='./logs/', 
                         help='where to store ckpts and logs')
+    # 输入数据路径
     parser.add_argument("--datadir", type=str, default='./data/llff/fern', 
                         help='input data directory')
 
-    # training options
+    # --- 训练配置参数 ---
+    # 粗MLP网络深度，默认：8
     parser.add_argument("--netdepth", type=int, default=8, 
                         help='layers in network')
+    # 粗MLP网络宽度，默认：256
     parser.add_argument("--netwidth", type=int, default=256, 
                         help='channels per layer')
+    # 精MLP网络深度，默认：8
     parser.add_argument("--netdepth_fine", type=int, default=8, 
                         help='layers in fine network')
+    # 精MLP网络宽度，默认：256
     parser.add_argument("--netwidth_fine", type=int, default=256, 
                         help='channels per layer in fine network')
+    # 随机采样射线的batch size
     parser.add_argument("--N_rand", type=int, default=32*32*4, 
                         help='batch size (number of random rays per gradient step)')
+    # 学习率
     parser.add_argument("--lrate", type=float, default=5e-4, 
                         help='learning rate')
+    # 学习率衰减参数
     parser.add_argument("--lrate_decay", type=int, default=250, 
                         help='exponential learning rate decay (in 1000 steps)')
+    # 并行处理的射线数量
     parser.add_argument("--chunk", type=int, default=1024*32, 
                         help='number of rays processed in parallel, decrease if running out of memory')
+    # 网络中并行处理的射线采样点的数量
     parser.add_argument("--netchunk", type=int, default=1024*64, 
                         help='number of pts sent through network in parallel, decrease if running out of memory')
+    # 不进行批量化，即一次只在一张图像中随机采样射线, 默认：false
     parser.add_argument("--no_batching", action='store_true', 
                         help='only take random rays from 1 image at a time')
+    # 不重载保存的网络权重，默认：false
     parser.add_argument("--no_reload", action='store_true', 
                         help='do not reload weights from saved ckpt')
+    # 为粗MLP网络加载特定权重npy文件
     parser.add_argument("--ft_path", type=str, default=None, 
                         help='specific weights npy file to reload for coarse network')
 
-    # rendering options
+    # --- 渲染配置参数 ---
+    # 粗网络每个射线上采样点数量，默认：64
     parser.add_argument("--N_samples", type=int, default=64, 
                         help='number of coarse samples per ray')
+    # 精网络每个射线上采样点数量，默认：0
     parser.add_argument("--N_importance", type=int, default=0,
                         help='number of additional fine samples per ray')
+    # 用于分层随机采样，设为0: 均匀采样，设为1: 分层随机采样
     parser.add_argument("--perturb", type=float, default=1.,
                         help='set to 0. for no jitter, 1. for jitter')
+    # 使用视角方向参与训练，组成5D输入，默认：false
     parser.add_argument("--use_viewdirs", action='store_true', 
                         help='use full 5D input instead of 3D')
+    # 编码参数，设为0: 默认编码，设为1: 无编码
     parser.add_argument("--i_embed", type=int, default=0, 
                         help='set 0 for default positional encoding, -1 for none')
+    # 用于3D位置编码，multires=log2(max_freq)，即2的指数
     parser.add_argument("--multires", type=int, default=10, 
                         help='log2 of max freq for positional encoding (3D location)')
+    # 用于2D视角方向编码，multires_views=log2(max_freq)，即2的指数
     parser.add_argument("--multires_views", type=int, default=4, 
                         help='log2 of max freq for positional encoding (2D direction)')
+    # 噪声标准差，用于sigma_a(体密度)的正则化，提高训练稳定性
     parser.add_argument("--raw_noise_std", type=float, default=0., 
                         help='std dev of noise added to regularize sigma_a output, 1e0 recommended')
-
+    # 加载已有权重，只进行渲染，不进行优化，默认：false
     parser.add_argument("--render_only", action='store_true', 
                         help='do not optimize, reload weights and render out render_poses path')
+    # 渲染测试集
     parser.add_argument("--render_test", action='store_true', 
                         help='render the test set instead of render_poses path')
+    # 降采样加速渲染
     parser.add_argument("--render_factor", type=int, default=0, 
                         help='downsampling factor to speed up rendering, set 4 or 8 for fast preview')
 
-    # training options
+    # 其他训练配置参数
+    # 图像中心区域训练步数
     parser.add_argument("--precrop_iters", type=int, default=0,
                         help='number of steps to train on central crops')
+    # 图像中心区域所占比例
     parser.add_argument("--precrop_frac", type=float,
                         default=.5, help='fraction of img taken for central crops') 
 
-    # dataset options
+    # --- 数据配置参数 ---
+    # 数据类型：llff / blender / deepvoxels
     parser.add_argument("--dataset_type", type=str, default='llff', 
                         help='options: llff / blender / deepvoxels')
+    # 从测试/验证集中仅用1/N个图像去测试
     parser.add_argument("--testskip", type=int, default=8, 
                         help='will load 1/N images from test/val sets, useful for large datasets like deepvoxels')
 
-    ## deepvoxels flags
+    # 物体形状：用于deepvoxels数据
     parser.add_argument("--shape", type=str, default='greek', 
                         help='options : armchair / cube / greek / vase')
 
-    ## blender flags
+    # 白色背景：用于blender数据集，blender合成物体的背景是白色
     parser.add_argument("--white_bkgd", action='store_true', 
                         help='set to render synthetic data on a white bkgd (always use for dvoxels)')
+    # 加载blender数据时降采样为一半，默认：false
     parser.add_argument("--half_res", action='store_true', 
                         help='load blender synthetic data at 400x400 instead of 800x800')
 
-    ## llff flags
+    # llff配置参数
+    # llff图像降采样因子 
     parser.add_argument("--factor", type=int, default=8, 
                         help='downsample factor for LLFF images')
+    # 不使用NDC坐标（适用于非正对拍摄场景）
     parser.add_argument("--no_ndc", action='store_true', 
                         help='do not use normalized device coordinates (set for non-forward facing scenes)')
+    # 视差线性采样（即使用逆深度，而不是深度）
     parser.add_argument("--lindisp", action='store_true', 
                         help='sampling linearly in disparity rather than depth')
+    # 数据是周围360度场景图片时，设为True
     parser.add_argument("--spherify", action='store_true', 
                         help='set for spherical 360 scenes')
+    # 每N张图像取1张作为llff测试集，paper中为8
     parser.add_argument("--llffhold", type=int, default=8, 
                         help='will take every 1/N images as LLFF test set, paper uses 8')
 
-    # logging/saving options
+    # --- 日志保存配置参数 ---
+    # 控制台打印频率
     parser.add_argument("--i_print",   type=int, default=100, 
                         help='frequency of console printout and metric loggin')
+    # tensorboard图像日志频率
     parser.add_argument("--i_img",     type=int, default=500, 
                         help='frequency of tensorboard image logging')
+    # 模型权重保存频率
     parser.add_argument("--i_weights", type=int, default=10000, 
                         help='frequency of weight ckpt saving')
+    # 测试集保存频率
     parser.add_argument("--i_testset", type=int, default=50000, 
                         help='frequency of testset saving')
+    # 渲染视频保存频率
     parser.add_argument("--i_video",   type=int, default=50000, 
                         help='frequency of render_poses video saving')
 
@@ -532,31 +575,46 @@ def config_parser():
 
 
 def train():
-
+    '''
+        训练主函数, 实现整个nerf pipeline
+    '''
+    # 读取参数（包括：配置文件，数据路径，log路径，实验名称，训练参数，渲染参数， 数据集参数等）
     parser = config_parser()
     args = parser.parse_args()
 
-    # Load data
-    K = None
+    K = None # 内参
+    # 加载LLFF数据
     if args.dataset_type == 'llff':
+        # images: 加载的图像，shape: [N_imgs, H, W, C]
+        # poses: 加载的相机poses，shape: [N_imgs, 3, 5], [R, t, hwf]->3x5
+        # bds: bounds，加载的边界，用于计算近和远平面距离
+        # render_poses: 待渲染的新视角
+        # i_test: 测试集ID，指定测试集的图像和pose
         images, poses, bds, render_poses, i_test = load_llff_data(args.datadir, args.factor,
                                                                   recenter=True, bd_factor=.75,
                                                                   spherify=args.spherify)
+        # hwf: image height, image width, camera focal length
         hwf = poses[0,:3,-1]
-        poses = poses[:,:3,:4]
+        # 去掉hwf后的3x4pose
+        poses = poses[:,:3,:4] # poses: [N_imgs, 3, 4]
         print('Loaded llff', images.shape, render_poses.shape, hwf, args.datadir)
+        # 将i_test转为list
         if not isinstance(i_test, list):
             i_test = [i_test]
-
+        # llffhold：每llffhold个图像选1个作为测试图像，paper中为8
         if args.llffhold > 0:
             print('Auto LLFF holdout,', args.llffhold)
+            # 更新后的测试集ID
             i_test = np.arange(images.shape[0])[::args.llffhold]
-
+        # i_val：验证集ID
         i_val = i_test
+        # i_train：训练集ID
         i_train = np.array([i for i in np.arange(int(images.shape[0])) if
                         (i not in i_test and i not in i_val)])
 
         print('DEFINING BOUNDS')
+        # 如果不使用NDC坐标，near和far值由bounds确定
+        # 否则，near=0,far=1
         if args.no_ndc:
             near = np.ndarray.min(bds) * .9
             far = np.ndarray.max(bds) * 1.
@@ -565,7 +623,7 @@ def train():
             near = 0.
             far = 1.
         print('NEAR FAR', near, far)
-
+    # 加载blender数据
     elif args.dataset_type == 'blender':
         images, poses, render_poses, hwf, i_split = load_blender_data(args.datadir, args.half_res, args.testskip)
         print('Loaded blender', images.shape, render_poses.shape, hwf, args.datadir)
@@ -607,18 +665,18 @@ def train():
         print('Unknown dataset type', args.dataset_type, 'exiting')
         return
 
-    # Cast intrinsics to right types
+    # 确保H,W为整数
     H, W, focal = hwf
     H, W = int(H), int(W)
     hwf = [H, W, focal]
-
+    # 得到内参矩阵K
     if K is None:
         K = np.array([
             [focal, 0, 0.5*W],
             [0, focal, 0.5*H],
             [0, 0, 1]
         ])
-
+    # 如果渲染测试集，将render_poses设置为i_test对应的相机pose
     if args.render_test:
         render_poses = np.array(poses[i_test])
 
